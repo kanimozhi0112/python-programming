66a17023dec00e616c54e119bbbab3d190ff487c
kanimozhi0112 <37370569+kanimozhi0112@users.noreply.github.com>
2018-03-18 14:43:05 +0530

    Create biggest of three numbers.py

---
+++ beginner level/biggest of three numbers.py
@@ -0,0 +1,10 @@
+a1=int(input("enter the num1"))
+a2=int(input("enter the num2"))
+a3=int(input("enter the num3"))
+if(a1>a2 and a1>a3):
+    print(a1)
+elif(a2>a1 and a2>a3):
+    print(a2)
+else:
+    print(a3)
+    
