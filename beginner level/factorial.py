7888d7764b369f61654ccc4237b167c6166054c8
kanimozhi0112 <37370569+kanimozhi0112@users.noreply.github.com>
2018-03-22 15:14:48 +0530

    Update factorial.py

--- beginner level/factorial.py
+++ beginner level/factorial.py
@@ -1,5 +1,5 @@
-num=int(input("number"))
+n=int(input("number"))
 factorial = 1
-for i in range(1,num + 1):
+for i in range(1,n + 1):
        factorial = factorial*i
-print("The factorial of",num,"is",factorial)
+print("The factorial of",n,"is",factorial)
