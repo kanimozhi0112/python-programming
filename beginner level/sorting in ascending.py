dad6d481e3e324594de4ad74022fa1c9d5070e31
kanimozhi0112 <37370569+kanimozhi0112@users.noreply.github.com>
2018-03-22 16:33:39 +0530

    Update sorting in ascending.py

--- beginner level/sorting in ascending.py
+++ beginner level/sorting in ascending.py
@@ -1,7 +1,7 @@
-a=[]
+s=[]
 n=int(input("number"))
 for i in range(1,n+1):
- b=int(input("enter the number")
- a.append(b)
-a.sort
-print(a)
+ b=int(input("enter the number"))
+ s.append(b)
+s.sort
+print(s)
