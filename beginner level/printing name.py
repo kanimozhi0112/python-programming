f0f91c051e4dfd111216732d05cb92eb70023a8e
kanimozhi0112 <37370569+kanimozhi0112@users.noreply.github.com>
2018-04-09 19:07:19 +0530

    Create printing name.py

---
+++ beginner level/printing name.py
@@ -0,0 +1,22 @@
+n=int(input("value"))
+for i in range(n):
+    print("enter the option")
+    print("1.loop")
+    print("2.exit")
+    k=int(input("enter the option:",))
+    if(k==1):
+     a=input("enter the name:")
+     e=input("enter the email:")
+     d=input("enter the dept:")
+     c=input("enter the clg:")
+     print("my name is",a)
+     print("my email is",e)
+     print("my dept is",d)
+     print("my clg is",c)
+     k=int(input("enter the option:",))
+    else:
+      exit
+      
+     
+ 
+
