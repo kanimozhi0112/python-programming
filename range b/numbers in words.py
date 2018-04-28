3bc724920d076d413760a0b530b7a4f73eb9d6d8
kanimozhi0112 <37370569+kanimozhi0112@users.noreply.github.com>
2018-04-28 11:20:41 +0530

    Create numbers in words.py

---
+++ range b/numbers in words.py
@@ -0,0 +1,24 @@
+a=int(input("value"))
+if(a==1):
+    print("one")
+elif(a==2):
+    print("two")
+elif(a==3):
+    print("three")
+elif(a==4):
+    print("four")
+elif(a==5):
+    print("five")
+elif(a==6):
+    print("six")
+elif(a==7):
+    print("seven")
+elif(a==8):
+    print("eight")
+elif(a==9):
+    print("nine")
+elif(a==10):
+    print("ten")
+else:
+    print("no")
+
